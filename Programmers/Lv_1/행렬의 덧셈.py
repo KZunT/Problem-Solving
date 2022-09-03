3f66eae25a6a88951ef081d68b392ede7c5aa23e
KZunT <rlawnsxo0224@naver.com>
2022-09-03 14:22:12 +0900

    Update 20220903

---
+++ Programmers/Lv_1/행렬의 덧셈.py
@@ -0,0 +1,11 @@
+def solution(arr1, arr2):
+    answer = []
+
+    for i in range(len(arr1)):
+        sum_list = []
+        for j in range(len(arr1[i])):
+            sum = arr1[i][j] + arr2[i][j]
+            sum_list.append(sum)
+        answer.append(sum_list)
+
+    return answer
