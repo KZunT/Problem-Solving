725492973e2db034c92944645b36d99e58573e43
KZunT <rlawnsxo0224@naver.com>
2022-09-15 17:19:17 +0900

    Update 20220915

---
+++ Programmers/Lv_1/이상한 문자 만들기.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = ''
+    list = s.split(" ")
+
+    for i in list:
+
+        for j in range(len(i)):
+
+            if j % 2 == 0:
+                answer = answer + i[j].upper()
+            else:
+                answer = answer + i[j].lower()
+
+        answer = answer + " "
+
+    idx = len(answer) - 1
+
+    return answer[:idx]
