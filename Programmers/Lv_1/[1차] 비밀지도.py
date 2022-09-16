531b06a6d42af2b84500a89abbb1a98c4e0d28f0
KZunT <rlawnsxo0224@naver.com>
2022-09-16 16:58:14 +0900

    Update 20220916

---
+++ Programmers/Lv_1/[1차] 비밀지도.py
@@ -0,0 +1,20 @@
+def solution(n, arr1, arr2):
+    answer = []
+
+    b_arr = []
+
+    for i, j in zip(arr1, arr2):
+        binary = i | j
+        b_arr.append(bin(binary)[2::])
+
+    for idx, b in enumerate(b_arr):
+        if len(b) < n:
+            b_arr[idx] = "0" * (n - len(b)) + b
+
+    for i in b_arr:
+        i = i.replace("1", "#")
+        i = i.replace("0", " ")
+        answer.append(i)
+
+    return answer
+
