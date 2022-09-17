ccfec952c736fcd8a50041bc29deb1fa76aa2598
KZunT <rlawnsxo0224@naver.com>
2022-09-17 14:11:23 +0900

    Update 20220917

---
+++ Programmers/Lv_1/두 개 뽑아서 더하기.py
@@ -0,0 +1,11 @@
+import itertools
+
+
+def solution(numbers):
+    answer = []
+    num_list = list(itertools.combinations(numbers, r=2))
+    sum_list = list(map(sum, num_list))
+    sum_list = list(set(sum_list))
+    sum_list.sort()
+
+    return sum_list
