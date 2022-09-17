2b6c14897023640c59a48d80a76bcb1b0a3dc98a
KZunT <rlawnsxo0224@naver.com>
2022-09-17 14:16:40 +0900

    Update 20220917

---
+++ Programmers/Lv_1/2016년.py
@@ -0,0 +1,6 @@
+import calendar
+def solution(a, b):
+    day_list = ["MON","TUE","WED","THU","FRI","SAT","SUN"]
+    idx = calendar.weekday(2016, a, b)
+    answer = day_list[idx]
+    return answer
