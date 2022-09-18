99ffce8e49e186cab1ffac4545ec40ca576f8981
KZunT <rlawnsxo0224@naver.com>
2022-09-18 18:45:52 +0900

    Update 20220918

--- Programmers/Lv_1/소수 찾기.py
+++ Programmers/Lv_1/소수 찾기.py
@@ -1,19 +1,6 @@
-#에라토스테네스의 체
-
-
+# 에라토스테네스의 체
 def solution(n):
     answer = []
-    #     for i in range(2,n+1):
-    #         prime = 1
-
-    #         for j in range(2,i):
-
-    #             if i % j == 0:
-
-    #                 prime = 0
-
-    #         if prime == 1:
-    #             answer.append(i)
 
     a = [False, False] + [True] * (n - 1)
 
