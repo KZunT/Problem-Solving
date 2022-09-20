449174cc955304184cc40d5542af4d338ea61bdf
KZunT <rlawnsxo0224@naver.com>
2022-09-20 15:12:51 +0900

    Update 20220920

---
+++ Programmers/Lv_1/실패율.py
@@ -0,0 +1,22 @@
+def solution(N, stages):
+    answer = []
+    fail = {}
+    for num in range(1, N + 1):
+        non_clear = 0
+        clear = 0
+
+        for stage in stages:
+            if stage >= num:
+                clear += 1
+            if stage == num:
+                non_clear += 1
+
+        if clear == 0:
+            fail[num] = 0
+        else:
+            fail[num] = non_clear / clear
+
+    fail = sorted(fail.items(), key=lambda x: x[1], reverse=True)
+    answer = list(dict(fail).keys())
+
+    return answer
