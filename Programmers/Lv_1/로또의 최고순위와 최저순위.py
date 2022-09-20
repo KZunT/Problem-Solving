3a862a056076451b2a4caa825ecbf921ae9384ff
KZunT <rlawnsxo0224@naver.com>
2022-09-20 15:16:52 +0900

    Update 20220920

---
+++ Programmers/Lv_1/로또의 최고순위와 최저순위.py
@@ -0,0 +1,16 @@
+def solution(lottos, win_nums):
+    cnt = 0
+    zero = 0
+
+    for num in lottos:
+        if num in win_nums:
+            cnt = cnt + 1
+
+    zero = lottos.count(0)
+
+    rank = [6, 6, 5, 4, 3, 2, 1]
+
+    answer = [rank[zero + cnt], rank[cnt]]
+
+    return answer
+
