94f4aa28351cf8ed5ac73ae14c321fe6227b4526
KZunT <rlawnsxo0224@naver.com>
2022-09-22 23:25:28 +0900

    Update 20220922

---
+++ Programmers/Lv_1/체육복.py
@@ -0,0 +1,18 @@
+def solution(n, lost, reserve):
+    answer = 0
+    s_list = list(range(1, n + 1))
+
+    dup = set(reserve) & set(lost)
+    reserve = list(set(reserve) - set(dup))
+    lost = list(set(lost) - set(dup))
+
+    for i in reserve:
+        if i - 1 in lost:
+            lost.remove(i - 1)
+            continue
+        elif i + 1 in lost:
+            lost.remove(i + 1)
+
+    answer = n - len(lost)
+
+    return answer
