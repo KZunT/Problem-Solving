10e446d772908335863aa6e014920801aa14f6a4
KZunT <rlawnsxo0224@naver.com>
2022-09-23 18:00:11 +0900

    Update 20220923

--- Programmers/Lv_1/완주하지 못한 선수.py
+++ Programmers/Lv_1/완주하지 못한 선수.py
@@ -12,7 +12,23 @@ def solution(participant, completion):
 
 # import collections
 #
-# 
+#
 # def solution(participant, completion):
 #     answer = collections.Counter(participant) - collections.Counter(completion)
 #     return list(answer.keys())[0]
+
+
+# hash 함수를 사용한 풀이 추가
+
+# def solution(participant, completion):
+#
+#     temp = 0
+#     dic = {}
+#     for part in participant:
+#         dic[hash(part)] = part
+#         temp += int(hash(part))
+#     for com in completion:
+#         temp -= hash(com)
+#     answer = dic[temp]
+#
+#     return answer
