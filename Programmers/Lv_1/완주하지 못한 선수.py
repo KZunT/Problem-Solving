49dc046c680e02e67d65a2fa6a526747e2a8767b
KZunT <rlawnsxo0224@naver.com>
2022-09-23 17:59:27 +0900

    Update 20220923

--- Programmers/Lv_1/완주하지 못한 선수.py
+++ Programmers/Lv_1/완주하지 못한 선수.py
@@ -7,3 +7,12 @@ def solution(participant, completion):
         if participant[i] != completion[i]:
             return participant[i]
     return participant[len(participant) - 1]
+
+# collections 사용한 풀이 추가
+
+# import collections
+#
+# 
+# def solution(participant, completion):
+#     answer = collections.Counter(participant) - collections.Counter(completion)
+#     return list(answer.keys())[0]
