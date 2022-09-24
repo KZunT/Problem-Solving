faaab2226de81b85080150065b2abcd3865e5621
KZunT <rlawnsxo0224@naver.com>
2022-09-24 11:58:03 +0900

    Update 20220924

---
+++ Coding-Test/20220924_LINE_1.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+def solution(queries: List[List[int]]) -> int:
+    answer = 0
+    arr_dict = {}
+    len_dict = {}
+
+    for query in queries:
+        arr_num = query[0]
+        add_ele = query[1]
+        arr_dict[arr_num] = 0
+        len_dict[arr_num] = 0
+
+    # print(arr_dict)
+
+    for query in queries:
+        arr_num = query[0]
+        add_ele = query[1]
+
+        # print(arr_num,"추가")
+
+        if arr_dict[arr_num] + add_ele > len_dict[arr_num]:
+            answer = answer + arr_dict[arr_num]
+            # print(arr_num,"복사",arr_dict[arr_num])
+
+            i = 0
+            while (True):
+                if 2 ** i >= arr_dict[arr_num] + add_ele:
+                    len_dict[arr_num] = 2 ** i
+                    break
+                i = i + 1
+
+        arr_dict[arr_num] = arr_dict[arr_num] + add_ele
+
+    # print(len_dict)
+
+    return answer
