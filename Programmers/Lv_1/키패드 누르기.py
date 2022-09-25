928728603ff809cef56966f566fe28dfec185c3f
KZunT <rlawnsxo0224@naver.com>
2022-09-25 18:49:11 +0900

    Update 20220925

--- Programmers/Lv_1/키패드 누르기.py
+++ Programmers/Lv_1/키패드 누르기.py
@@ -1,6 +1,6 @@
 def solution(numbers, hand):
     answer = ''
-    pad = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['*', 0, '#']]
+    pad = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['*', 0, '#']] # 키패드 생성
     current_left = '*'
     current_right = '#'
 
@@ -13,10 +13,10 @@ def solution(numbers, hand):
                 r_coord = idx, row.index(current_right)
             if current_left in row:
                 l_coord = idx, row.index(current_left)
-
+        # 번호간 거리 계산 하여 우선순위 설정
         r_d = abs(num_coord[0] - r_coord[0]) + abs(num_coord[1] - r_coord[1])
         l_d = abs(num_coord[0] - l_coord[0]) + abs(num_coord[1] - l_coord[1])
-
+        # 우선순위대로 입력
         if num in [1, 4, 7]:
             answer = answer + "L"
             current_left = num
