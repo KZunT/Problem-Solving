2c6f344646135af0d13b618fc81a8de2530ab014
KZunT <rlawnsxo0224@naver.com>
2022-09-28 16:54:58 +0900

    Update 20220928

--- Programmers/Lv_1/성격 유형 검사하기.py
+++ Programmers/Lv_1/성격 유형 검사하기.py
@@ -32,3 +32,26 @@ def solution(survey, choices):
         answer += 'N'
 
     return answer
+
+# 순서쌍으로 나눈 코드 첨부
+
+# def solution(survey, choices):
+#
+#     my_dict = {"RT":0,"CF":0,"JM":0,"AN":0}
+#     for A,B in zip(survey,choices):
+#         if A not in my_dict.keys():
+#             A = A[::-1]
+#             my_dict[A] -= B-4
+#         else:
+#             my_dict[A] += B-4
+#
+#     result = ""
+#     for name in my_dict.keys():
+#         if my_dict[name] > 0:
+#             result += name[1]
+#         elif my_dict[name] < 0:
+#             result += name[0]
+#         else:
+#             result += sorted(name)[0]
+#
+#     return result
