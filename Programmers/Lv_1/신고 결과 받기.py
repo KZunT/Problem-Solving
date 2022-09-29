12ab6b92a5c416f91321db78efc35421ee0e2d7d
KZunT <rlawnsxo0224@naver.com>
2022-09-29 21:40:12 +0900

    Update 20220929

---
+++ Programmers/Lv_1/신고 결과 받기.py
@@ -0,0 +1,18 @@
+def solution(id_list, report, k):
+
+    answer = [0 for i in range(len(id_list))]
+    report_dict = {id: [] for id in id_list}
+
+    report = list(set(report))
+
+    report_cnt = []
+
+    for rep in report:
+        report_dict[rep.split(' ')[1]].append(rep.split(' ')[0])
+
+    for key, value in report_dict.items():
+        if len(value) >= k:
+            for v in value:
+                answer[id_list.index(v)] = answer[id_list.index(v)] + 1
+
+    return answer
