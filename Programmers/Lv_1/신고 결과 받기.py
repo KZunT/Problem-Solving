dcbe4a58d172fcf37a779cfd5a064c856ae916ad
KZunT <rlawnsxo0224@naver.com>
2022-09-29 21:41:43 +0900

    Update 20220929

--- Programmers/Lv_1/신고 결과 받기.py
+++ Programmers/Lv_1/신고 결과 받기.py
@@ -14,3 +14,19 @@ def solution(id_list, report, k):
                 answer[id_list.index(v)] = answer[id_list.index(v)] + 1
 
     return answer
+
+# 속도 계산 코드 추가
+
+# def solution(id_list, report, k):
+#
+#     answer = [0] * len(id_list)
+#     reports = {x : 0 for x in id_list}
+# 
+#     for r in set(report):
+#         reports[r.split()[1]] += 1
+#
+#     for r in set(report):
+#         if reports[r.split()[1]] >= k:
+#             answer[id_list.index(r.split()[0])] += 1
+#
+#     return answer
