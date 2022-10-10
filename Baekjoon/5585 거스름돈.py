f92537c0bb5d4e2a5a65a3d8e33936105ec15652
KZunT <rlawnsxo0224@naver.com>
2022-10-10 18:13:16 +0900

    Update 20221010

--- Baekjoon/5585 거스름돈.py
+++ Baekjoon/5585 거스름돈.py
@@ -4,13 +4,17 @@ coin_list = [500, 100, 50, 10, 5, 1]
 
 coin_cnt = 0
 
-while(True):
+# while(True):
+#
+#     coin = [x for x in coin_list if x <= pay]
+#     pay = pay - max(coin)
+#     coin_cnt += 1
+#
+#     if pay == 0:
+#         break
 
-    coin = [x for x in coin_list if x <= pay]
-    pay = pay - max(coin)
-    coin_cnt += 1
-
-    if pay == 0:
-        break
+for coin in coin_list:
+    coin_cnt += pay // coin
+    pay = pay % coin
 
 print(coin_cnt)
