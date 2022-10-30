9b897b726ac5acd45dbc59be2993bfa525a2d414
KZunT <rlawnsxo0224@naver.com>
2022-10-30 14:37:53 +0900

    Update 20221030

--- Baekjoon/11652 카드.py
+++ Baekjoon/11652 카드.py
@@ -10,4 +10,19 @@
 # maxi = max(num_list, key=num_list.count)
 #
 # print(maxi)
+import sys
 
+N = int(input())
+
+num_dict = {}
+
+for _ in range(N):
+    num = int(sys.stdin.readline())  # input() 사용하면 시간초과..
+    if num in num_dict:
+        num_dict[num] += 1
+    else:
+        num_dict[num] = 1
+
+num = sorted(num_dict.items(), key=lambda x: (-x[1], x[0]))  # -x 는 내림차순 x는 오름차순의 이미
+
+print(num[0][0])
