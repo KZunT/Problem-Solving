8d0b40e5237bd6b31baa227ab2c2c7ab9c56d2c1
KZunT <rlawnsxo0224@naver.com>
2022-10-30 14:28:34 +0900

    Update 20221030

---
+++ Baekjoon/11652 카드.py
@@ -0,0 +1,13 @@
+# 시간초과 난 코드
+
+# N = int(input())
+#
+# num_list = []
+#
+# for _ in range(N):
+#     num_list.append(int(input()))
+#
+# maxi = max(num_list, key=num_list.count)
+#
+# print(maxi)
+
