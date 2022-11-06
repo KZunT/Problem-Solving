02ebb7acfb2420ce0b9cfff04477c50a58b54693
KZunT <rlawnsxo0224@naver.com>
2022-11-06 17:38:12 +0900

    Update 20221106

---
+++ Baekjoon/2740 행렬 곱셈.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+
+A = [list(map(int, input().split())) for i in range(N)]
+
+M, K = map(int, input().split())
+
+B = [list(map(int, input().split())) for i in range(M)]
+
+for i in range(N):
+    result = []
+    for l in range(K):
+        a = 0
+        for j in range(M):
+            a += A[i][j] * B[j][l]
+        result.append(a)
+
+    print(*result)
