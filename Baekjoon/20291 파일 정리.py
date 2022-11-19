f7c5aab37b9d8676195c7ba337441d3ecd117ee2
KZunT <rlawnsxo0224@naver.com>
2022-11-19 13:16:35 +0900

    Update 20221119

---
+++ Baekjoon/20291 파일 정리.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+file = dict()
+
+for _ in range(N):
+    extend = (input().split('.'))[1]
+    if extend not in file:
+        file[extend] = 1
+    else:
+        file[extend] += 1
+
+sort_file = sorted(file.items())
+
+for key, value in sort_file:
+    print(key, value)
