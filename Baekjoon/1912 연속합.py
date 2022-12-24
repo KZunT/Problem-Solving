1452f86f69f49808463f2a26f81b5c91e81c0ada
KZunT <rlawnsxo0224@naver.com>
2022-12-24 15:25:37 +0900

    Update 20221224

---
+++ Baekjoon/1912 연속합.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+arr = list(map(int, input().split()))
+
+dp = []
+
+dp.append(arr[0])
+
+
+for i in range(1, N):
+    dp.append(max((dp[i - 1] + arr[i]), arr[i]))
+
+
+print(max(dp))
