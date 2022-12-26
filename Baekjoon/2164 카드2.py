286886fb429b4d222671790a57d9b49ffd0fefaa
KZunT <rlawnsxo0224@naver.com>
2022-12-26 20:34:33 +0900

    Update 20221226

--- Baekjoon/2164 카드2.py
+++ Baekjoon/2164 카드2.py
@@ -1,23 +1,35 @@
-from collections import deque
+# from collections import deque
+#
+# N = int(input())
+#
+# q = deque()
+#
+# for i in range(1, N + 1):
+#     q.append(i)
+#
+# while True:
+#     if len(q) == 1:
+#         break
+#
+#     q.popleft()
+#
+#     if len(q) == 1:
+#         break
+#
+#     q.append(q.popleft())
+#
+# print(q[0])
 
-N = int(input())
 
-q = deque()
+#시간이 빠른 코드 추가 (규칙을 이용)
 
-for i in range(1, N + 1):
-    q.append(i)
-
+n = int(input())
+num = 2
 while True:
-    if len(q) == 1:
+    if n == 1 or n == 2:
+        print(n)
         break
-
-    q.popleft()
-
-    if len(q) == 1:
+    num *= 2
+    if num >= n:
+        print((n - (num // 2)) * 2)
         break
-
-    q.append(q.popleft())
-
-print(q[0])
-
-
