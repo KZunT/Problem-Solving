08b840fa1c4e27efb995d7216302d43e2356520d
KZunT <rlawnsxo0224@naver.com>
2022-12-29 20:20:56 +0900

    Update 20221229

--- Baekjoon/4948 베르트랑 공준.py
+++ Baekjoon/4948 베르트랑 공준.py
@@ -18,3 +18,25 @@ while True:
         print(1)
     else:
         print(brt(num))
+
+# 시간초과 해결 코드.. 어째서?
+# def getPrimaryNum_Eratos(N):
+#     nums = [True] * (N)
+#     for i in range(2, int(N ** 0.5) + 1):
+#         if nums[i] == True:
+#             for j in range(i + i, N, i):
+#                 nums[j] = False
+#
+#     return [i for i in range(2, N) if nums[i] == True]
+#
+#
+# while True:
+#     N = int(input())
+#
+#     if N == 0:
+#         break
+#
+#     prime_list = getPrimaryNum_Eratos(2 * N + 1)
+#     answer_list = [num for num in prime_list if num > N]
+#
+#     print(len(answer_list))
