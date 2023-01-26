3406890817e9f9225862daaf9da091113fbb4108
KZunT <rlawnsxo0224@naver.com>
2023-01-26 14:30:22 +0900

    Update 20230126

---
+++ Baekjoon/15650 N과 M (2).py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+
+stack = []
+
+def dfs(start):
+        if len(stack) == M:
+            print(' '.join(map(str, stack)))
+            return
+
+        for i in range(start,N+1):
+            if i not in stack:
+                stack.append(i)
+                dfs(i+1)
+                stack.pop()
+
+dfs(1)
