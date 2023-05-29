09f97f588a73746958491c34ce3451d074fb5ada
KZunT <rlawnsxo0224@naver.com>
2023-05-29 21:32:30 +0900

    Update 20230529

---
+++ Baekjoon/1251 단어 나누기.py
@@ -0,0 +1,12 @@
+word = input()
+answer = []
+
+for i in range(1, len(word) - 1):
+    for j in range(i + 1, len(word)):
+        first = word[:i][::-1]
+        second = word[i:j][::-1]
+        third = word[j:][::-1]
+
+        answer.append("".join(first + second + third))
+
+print(min(answer))
