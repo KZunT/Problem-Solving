015c2245872fe64994b9c65c741c2f43ec13efa8
KZunT <rlawnsxo0224@naver.com>
2023-06-04 16:45:36 +0900

    Update 20230604

--- Programmers/Lv_2/두 원 사이의 정수 쌍.py
+++ Programmers/Lv_2/두 원 사이의 정수 쌍.py
@@ -8,20 +8,24 @@
 반지름은 정수로 주어진다.
 
 흠.. 점이 원의 넓이 안에 있으면 되겠다. 3:15
+(x - a)^2 + (y - b)^2 < r^2
 
-엣지 케이스 해결을 해야한다. (두 점이 모두 정수일 때)
+엣지 케이스 해결을 해야한다. (점이 정수일 때)
 
 시간이 좀 오래걸린다. 어떻게 줄여야 할지는 모르겠다.
 
-문제 풀이 3:35
+문제 풀이 3:55
 '''
 
-from math import sqrt
+import math
+
 
 def solution(r1, r2):
     answer = 0
-    for i in range(0, r1):
-        answer += int(sqrt(r2**2 - i**2)) - int(sqrt(r1**2 - i**2 - 1))
-    for j in range(r1, r2):
-        answer += int(sqrt(r2**2 - j**2))
+    for x in range(1, r2 + 1):
+        r1_point = 0  # r1의 원 교점
+        if x <= r1:
+            r1_point = math.ceil((r1 ** 2 - x ** 2) ** 0.5)  # x 범위가 r1을 벗어나면 교점이 없다.
+        r2_point = math.floor((r2 ** 2 - x ** 2) ** 0.5)  # r2의 원 교점
+        answer += (r2_point - r1_point + 1)  # 두 원 사이의 정수 좌표 구하기
     return answer * 4
