fb32ee43d9c3eda4b192d19485642c8c7f2ffd92
KZunT <rlawnsxo0224@naver.com>
2023-07-12 20:57:02 +0900

    Update 20230712

---
+++ Baekjoon/26069 붙임성 좋은 총총이.py
@@ -0,0 +1,13 @@
+N = int(input())
+dance = {'ChongChong'}
+
+for _ in range(N):
+    A, B = input().rstrip().split()
+
+    if A in dance:
+        dance.add(B)
+
+    if B in dance:
+        dance.add(A)
+
+print(len(dance))
