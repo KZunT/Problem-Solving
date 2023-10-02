2304cd508ebd96b1cf3f918674e3e155979128b5
KZunT <rlawnsxo0224@naver.com>
2023-10-02 17:39:56 +0900

    Update 20231002

---
+++ Baekjoon/11060 점프점프.py
@@ -0,0 +1,13 @@
+N = int(input())
+numbers = list(map(int, input().split()))
+
+dp = [N + 1] * N
+dp[0] = 0
+
+for i in range(N):
+    for j in range(1, numbers[i] + 1):
+        if i + j >= N:
+            break
+        dp[i + j] = min(dp[i + j], dp[i] + 1)
+
+print(dp[N - 1] if dp[N - 1] != N + 1 else -1)
