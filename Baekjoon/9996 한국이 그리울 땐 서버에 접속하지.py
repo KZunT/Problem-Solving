12f92a86326864cc360838bcc7287701ceebf147
KZunT <rlawnsxo0224@naver.com>
2023-10-04 22:43:47 +0900

    Update 20231004

---
+++ Baekjoon/9996 한국이 그리울 땐 서버에 접속하지.py
@@ -0,0 +1,14 @@
+N = int(input())
+pattern = input().split("*")
+length = len(pattern[0]) + len(pattern[1])
+
+for _ in range(N):
+    file = input()
+    if length > len(file):
+        print("NE")
+
+    else:
+        if pattern[0] == file[:len(pattern[0])] and pattern[1] == file[-len(pattern[1]):]:
+            print("DA")
+        else:
+            print("NE")
