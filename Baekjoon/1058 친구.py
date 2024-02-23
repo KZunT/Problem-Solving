5dfec9542f87f6a0926bf61c896272b453aac917
KZunT <rlawnsxo0224@naver.com>
2024-02-23 23:37:05 +0900

    Update 20240223

---
+++ Baekjoon/1058 친구.py
@@ -0,0 +1,19 @@
+N = int(input())
+friends = [list(input()) for _ in range(N)]
+
+connected = [[0] * N for _ in range(N)]
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if i == j:
+                continue
+            if friends[i][j] == "Y" or (friends[i][k] == "Y" and friends[k][j] == "Y"):
+                connected[i][j] = 1
+
+answer = 0
+
+for con in connected:
+    answer = max(answer, sum(con))
+
+print(answer)
