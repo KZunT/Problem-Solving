1673517c756c6c89ac7bc3d2ebc941bd59193592
KZunT <rlawnsxo0224@naver.com>
2024-03-02 21:57:09 +0900

    Update 20240302

---
+++ Baekjoon/4344 평균은 넘겠지.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+for _ in range(N):
+    scores = list(map(int, input().split()))
+    avg = sum(scores[1:]) / scores[0]
+
+    cnt = 0
+    for i in scores[1:]:
+        if i > avg:
+            cnt += 1
+
+    per = (cnt / scores[0]) * 100
+    print('%.3f' % per + '%')
